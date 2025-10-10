9ba5d6afb221e08c6bd8f00337cde3ce447d3ff4
dawa33 <contact@da-wa.com>
2025-10-10 16:39:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.getElementById("downloadBtn").addEventListener("click", () => {
 window.open("https://www.curseforge.com/minecraft/mc-mods/item-plus-ex", "_blank");
 });
+document.getElementById("downloadBtn_modrinth").addEventListener("click", () => {
+window.open("https://modrinth.com/mod/item-plus-ex", "_blank");
+});
